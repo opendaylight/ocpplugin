908167014a1773f585233888d4febd327b4c2949
Richard <m8809301@msn.com>
2016-05-06 01:12:29 -0700

    Add a simple OCP agent for the following purposes:
    1.) Interact with the ocpplugin/controller during system tests
    2.) Demonstrate protocol communication occurring between the OCP agent and ocpplugin/controller
    
    Developer's Certificate of Origin 1.1
    
            By making a contribution to this project, I certify that:
    
            (a) The contribution was created in whole or in part by me and I
                have the right to submit it under the open source license
                indicated in the file; or
    
            (b) The contribution is based upon previous work that, to the best
                of my knowledge, is covered under an appropriate open source
                license and I have the right under that license to submit that
                work with modifications, whether created in whole or in part
                by me, under the same open source license (unless I am
                permitted to submit under a different license), as indicated
                in the file; or
    
            (c) The contribution was provided directly to me by some other
                person who certified (a), (b) or (c) and I have not modified
                it.
    
            (d) I understand and agree that this project and the contribution
                are public and that a record of the contribution (including all
                personal information I submit with it, including my sign-off) is
                maintained indefinitely and may be redistributed consistent with
                this project or the open source license(s) involved.
    
    Change-Id: I63483ada95e99b2415322c5bf502f69069099111
    Signed-off-by: Richard <m8809301@msn.com>

---
+++ simple-agent/src/main/java/org/opendaylight/ocpplugin/OcpAgent.java
@@ -0,0 +1,244 @@
+/*
+ * Copyright (c) 2015 Foxconn Corporation and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ocpplugin;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.*;
+import java.net.*;
+
+/*
+ * Simple OCP Agent
+ *
+ * @author Richard Chien <richard.chien@foxconn.com>
+ */
+public class OcpAgent 
+{
+    private static final String MSG_HELLO_ACK = "helloAck";
+    private static final String MSG_SET_TIME_REQ = "setTimeReq";
+    private static final String MSG_HEALTH_CHECK_REQ = "healthCheckReq";
+    private static final String MSG_GET_PARAM_REQ = "getParamReq";
+
+    private static final String ELM_MSG_UID = "msgUID";
+    private static final String ELM_RESULT = "result";
+    private static final String ELM_OBJECT = "obj";
+    private static final String ELM_PARAMETER = "param";
+
+    private DataOutputStream out;
+
+    private String vendorId = "unknown";
+    private String serialNumber = "unknown";
+
+    private String uid;
+    private String value;
+    private String result;
+    private String obj;
+    private String param;
+
+   
+    public OcpAgent(String vendorId, String serialNumber) {
+        this.vendorId = vendorId;
+        this.serialNumber = serialNumber;
+    }
+
+    public void createConnection(String controllerIpAddr, int portNumber) {
+        try {
+            Socket socket = new Socket(controllerIpAddr, portNumber);
+            out = new DataOutputStream(socket.getOutputStream());
+            InputStream is = socket.getInputStream();
+            DataInputStream dis = new DataInputStream(is);
+      
+            Thread.sleep(500);
+            sendHello();
+         
+            String msg = "";
+            while (true) {
+                int count = is.available();
+                if (count > 0) {
+                    byte[] bs = new byte[count];
+                    dis.read(bs);
+                    msg += new String(bs);
+                    if (msg.contains("</msg>")) {
+                        parseDocument(msg);
+                        msg = "";
+                    }
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Unknown host " + controllerIpAddr);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + controllerIpAddr);
+            System.exit(1);
+        } catch (InterruptedException e) {
+            System.err.println("Couldn't sleep for 500 ms");
+            System.exit(1);
+        }
+    }
+
+    private void parseDocument(String msg) {
+        try {
+            XMLInputFactory factory = XMLInputFactory.newFactory();
+            XMLStreamReader reader = factory.createXMLStreamReader(new StringReader(msg));
+            //Now iteration
+            while (reader.hasNext()) {
+                parseEvent(reader);
+                reader.next();
+            }
+            reader.close();
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseEvent(XMLStreamReader reader) {
+        switch (reader.getEventType()) {
+            case XMLStreamConstants.START_DOCUMENT:
+                //System.out.println("Start of document");
+                break;
+            case XMLStreamConstants.START_ELEMENT:
+                //System.out.println("Start element = " + reader.getLocalName());
+                break;
+            case XMLStreamConstants.CHARACTERS:
+                int beginIndex = reader.getTextStart();
+                int endIndex = reader.getTextLength();
+                value = new String(reader.getTextCharacters(),
+                        beginIndex,
+                        endIndex).trim();
+                //if (!value.equalsIgnoreCase(""))
+                //    System.out.println("Value = " + value);
+                break;
+            case XMLStreamConstants.END_ELEMENT:
+                //System.out.println("End element = " + reader.getLocalName());
+                
+                if (reader.getLocalName().equals(ELM_MSG_UID)) {
+                    uid = value;
+                }
+                else if (reader.getLocalName().equals(ELM_RESULT)) {
+                    result = value;
+                }
+
+                else if (reader.getLocalName().equals(MSG_HELLO_ACK)) {
+                    System.out.println("\n\nhello ack received (result = " + result + ")");
+                }
+                else if (reader.getLocalName().equals(MSG_SET_TIME_REQ)) {
+                    sendSetTimeResp();
+                }
+                else if (reader.getLocalName().equals(MSG_HEALTH_CHECK_REQ)) {
+                    sendHealthCheckResp(); 
+                }
+                else if (reader.getLocalName().equals(MSG_GET_PARAM_REQ)) {
+                    sendGetParamResp();
+                }
+                break;
+            case XMLStreamConstants.COMMENT:
+                if (reader.hasText())
+                    System.out.print(reader.getText());
+                break;
+        }
+    }
+
+    private void sendHello() {
+        String msg = "<msg xmlns=\"http://uri.etsi.org/ori/002-2/v4.1.1\">\n" +
+                     "<header>\n" +
+                     "<msgType>IND</msgType>\n" +
+                     "<msgUID>0</msgUID>\n" +
+                     "</header>\n" + 
+                     "<body>\n" +
+                     "<helloInd>\n" +
+                     "<version>4.1.1</version>\n" +
+                     "<vendorId>" + vendorId + "</vendorId>\n" +
+                     "<serialNumber>" + serialNumber + "</serialNumber>\n" +
+                     "</helloInd>\n" +
+                     "</body>\n" +
+                     "</msg>";
+        try {
+            out.writeBytes(msg);
+            System.out.println("\n\n" + msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendSetTimeResp() {
+        String msg = "<msg xmlns=\"http://uri.etsi.org/ori/002-2/v4.1.1\">" +
+                     "<header>" +
+                     "<msgType>RESP</msgType>" +
+                     "<msgUID>" + uid + "</msgUID>" +
+                     "</header>" +
+                     "<body>" +
+                     "<setTimeResp>" +
+                     "<result>SUCCESS</result>" +
+                     "</setTimeResp>" +
+                     "</body>" +
+                     "</msg>";
+        try {
+            out.writeBytes(msg);
+            System.out.println("\n\n" + msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendHealthCheckResp() {
+        String msg = "<msg xmlns=\"http://uri.etsi.org/ori/002-2/v4.1.1\">" +
+                     "<header>" +
+                     "<msgType>RESP</msgType>" +
+                     "<msgUID>" + uid + "</msgUID>" +
+                     "</header>" +
+                     "<body>" +
+                     "<healthCheckResp>" +
+                     "<result>SUCCESS</result>" +
+                     "</healthCheckResp>" +
+                     "</body>" +
+                     "</msg>";
+        try {
+            out.writeBytes(msg);
+            System.out.println("\n\n" + msg);
+        } catch (IOException e) {
+            e.printStackTrace(); 
+        }
+    }
+
+    private void sendGetParamResp() {
+        String msg = "<msg xmlns=\"http://uri.etsi.org/ori/002-2/v4.1.1\">" +
+                     "<header>" +
+                     "<msgType>RESP</msgType>" +
+                     "<msgUID>" + uid + "</msgUID>" +
+                     "</header>" +
+                     "<body>" +
+                     "<getParamResp>" +
+                     "<result>SUCCESS</result>" +
+                     "<obj objID=\"RE:0\">" +
+                     "<param name=\"vendorID\">" + vendorId + "</param>" +
+                     "</obj>" + 
+                     "</getParamResp>" +
+                     "</body>" +
+                     "</msg>";
+        try {
+            out.writeBytes(msg);
+            System.out.println("\n\n" + msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            System.err.println(
+                "Usage: java org.opendaylight.ocpplugin.OcpAgent <controller's ip address> <port number> <vendor id> <serial number>");
+            System.exit(1);
+        }
+        System.out.println("\n\nStarting OCP Agent..");
+        OcpAgent agent = new OcpAgent(args[2], args[3]);
+        agent.createConnection(args[0], Integer.parseInt(args[1]));
+    }
+
+}
